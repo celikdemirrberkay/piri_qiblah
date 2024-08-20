e9d090526aeb13e61aacbb71edaf404424b73179
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-08-20 14:44:45 +0300

    Initialize commit

---
+++ lib/piri_qiblah.dart
@@ -0,0 +1,3 @@
+library piri_qiblah;
+
+export 'src/piri_qiblah.dart';
---
+++ lib/src/piri_qiblah.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class PiriQiblah extends StatefulWidget {
+  const PiriQiblah({super.key});
+
+  @override
+  State<PiriQiblah> createState() => _PiriQiblahState();
+}
+
+class _PiriQiblahState extends State<PiriQiblah> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
