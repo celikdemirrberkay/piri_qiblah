b9f18d88b750ee1b9fb50bad6d5631d844c665ae
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-08-20 21:29:23 +0300

    Continues qibla calculations via example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
@@ -12,114 +21,116 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      /// 39.75649757032112, 37.05508957748691
+      home: const PiriQiblah(
+        userLatitude: 39.7602078065278,
+        userLongitude: 37.05381501948753,
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+@immutable
+final class PiriQiblah extends StatefulWidget {
+  ///
+  const PiriQiblah({
+    super.key,
+    required this.userLatitude,
+    required this.userLongitude,
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final double userLatitude;
+  final double userLongitude;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PiriQiblahState createState() => _PiriQiblahState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PiriQiblahState extends State<PiriQiblah> {
+  double? qiblaDirection;
+  double initialDirection = 0;
+  final _sensors = Sensors();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    qiblaDirection = calculateQiblaDirection(widget.userLatitude, widget.userLongitude);
+    _sensors.magnetometerEventStream().listen((event) {
+      setState(() {
+        initialDirection = calculateDeviceDirection(event);
+      });
     });
+  }
+
+  /// Calculate device direction
+  double calculateDeviceDirection(MagnetometerEvent event) {
+    // Manyetik alan verilerini kullanarak cihazın yönünü hesaplayın
+    double angle = atan2(event.y, event.x) * (180 / pi);
+    return (angle + 360) % 360; // Negatif açıları düzelt
+  }
+
+  /// Calculate Qibla direction
+  double calculateQiblaDirection(double latitude, double longitude) {
+    const double makkahLatitude = 21.4225;
+    const double makkahLongitude = 39.8262;
+
+    double latDifference = (makkahLatitude - latitude).toRadians();
+    double lonDifference = (makkahLongitude - longitude).toRadians();
+
+    double y = sin(lonDifference) * cos(makkahLatitude.toRadians());
+    double x = cos(latitude.toRadians()) * sin(makkahLatitude.toRadians()) - sin(latitude.toRadians()) * cos(makkahLatitude.toRadians()) * cos(lonDifference);
+
+    /// Kıble yönü hesaplamasında, kullanıcının bulunduğu konum ile Mekke arasındaki coğrafi
+    /// farktan bir açı elde etmek için atan2 fonksiyonu kullanılır.
+    /// Bu açı, cihazın hangi yöne bakması gerektiğini belirler.
+    double qiblaDirection = atan2(y, x).toDegrees();
+
+    return (qiblaDirection + 360) % 360;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text('Piri Qiblah')),
+      body: SafeArea(
+        child: qiblaDirection == null
+            ? const CircularProgressIndicator()
+            : Transform.rotate(
+                angle: (initialDirection - qiblaDirection!) * pi / 180,
+                child: Stack(
+                  children: [
+                    Center(
+                      child: SvgPicture.asset(
+                        'assets/compass.svg',
+                        width: 300,
+                        height: 300,
+                      ),
+                    ),
+                    Center(
+                      child: Transform.rotate(
+                        angle: qiblaDirection! * pi / 180 * -1,
+                        child: const Icon(
+                          Icons.navigation,
+                          size: 100,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+extension AngleConversion on double {
+  double toRadians() => this * pi / 180;
+  double toDegrees() => this * 180 / pi;
 }
--- lib/src/piri_qiblah.dart
+++ lib/src/piri_qiblah.dart
@@ -1,15 +1,70 @@
-import 'package:flutter/widgets.dart';
+import 'dart:math';
 
-class PiriQiblah extends StatefulWidget {
-  const PiriQiblah({super.key});
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+/// PiriQiblah is a widget that shows the direction of the Qiblah.
+@immutable
+final class PiriQiblah extends StatefulWidget {
+  ///
+  const PiriQiblah({
+    super.key,
+    required this.langitude,
+    required this.longitude,
+  });
+
+  /// The langitude and longitude
+  final double langitude;
+  final double longitude;
 
   @override
   State<PiriQiblah> createState() => _PiriQiblahState();
 }
 
 class _PiriQiblahState extends State<PiriQiblah> {
+  double _direction = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Sensors().magnetometerEventStream().listen((MagnetometerEvent event) {
+      setState(() {
+        _direction = event.x;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Transform.rotate(
+          angle: _direction * pi / 180 * -1,
+          child: Icon(
+            Icons.navigation,
+            size: 100,
+          ),
+        ),
+      ),
+    );
   }
+}
+
+double calculateQiblaDirection(double latitude, double longitude) {
+  const double makkahLatitude = 21.4225;
+  const double makkahLongitude = 39.8262;
+
+  double lonDifference = (makkahLongitude - longitude).toRadians();
+
+  double y = sin(lonDifference) * cos(makkahLatitude.toRadians());
+  double x = cos(latitude.toRadians()) * sin(makkahLatitude.toRadians()) - sin(latitude.toRadians()) * cos(makkahLatitude.toRadians()) * cos(lonDifference);
+
+  double qiblaDirection = atan2(y, x).toDegrees();
+
+  return (qiblaDirection + 360) % 360;
+}
+
+extension AngleConversion on double {
+  double toRadians() => this * pi / 180;
+  double toDegrees() => this * 180 / pi;
 }
