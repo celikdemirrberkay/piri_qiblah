96cab980a897cd6934f2e145b98fc3f1dd91aa03
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-08-21 12:47:58 +0300

    In example directory trying flutter_qiblah

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,8 +2,9 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:sensors_plus/sensors_plus.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,10 +29,7 @@ class MyApp extends StatelessWidget {
       ),
 
       /// 39.75649757032112, 37.05508957748691
-      home: const PiriQiblah(
-        userLatitude: 39.7602078065278,
-        userLongitude: 37.05381501948753,
-      ),
+      home: const PiriQiblah(),
     );
   }
 }
@@ -39,59 +37,57 @@ class MyApp extends StatelessWidget {
 @immutable
 final class PiriQiblah extends StatefulWidget {
   ///
-  const PiriQiblah({
-    super.key,
-    required this.userLatitude,
-    required this.userLongitude,
-  });
-
-  final double userLatitude;
-  final double userLongitude;
+  const PiriQiblah({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _PiriQiblahState createState() => _PiriQiblahState();
 }
 
-class _PiriQiblahState extends State<PiriQiblah> {
-  double? qiblaDirection;
-  double initialDirection = 0;
-  final _sensors = Sensors();
+class _PiriQiblahState extends State<PiriQiblah> with TickerProviderStateMixin {
+  /// Animation properties for needles
+  late Animation<double>? animationForNeedle;
+  late AnimationController? _animationControllerForNeedle;
+
+  /// Animation properties for background compass view
+  late Animation<double>? animationForBackgroundCompass;
+  late AnimationController? _animationControllerForBackgroundCompass;
+
+  double beginForNeedle = 0;
+  double beginForCompass = 0;
 
   @override
   void initState() {
+    /// Check and Request location permission for qiblah
+    requestLocationPermissionForQiblah();
+
+    /// Animation for needles
+    _animationControllerForNeedle = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    animationForNeedle = Tween<double>(
+      begin: 0.0,
+      end: 360.0,
+    ).animate(_animationControllerForNeedle!);
+
+    /// Animation for background  compass view
+    _animationControllerForBackgroundCompass = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 500),
+    );
+    animationForBackgroundCompass = Tween<double>(
+      begin: 0.0,
+      end: 0.0,
+    ).animate(_animationControllerForBackgroundCompass!);
     super.initState();
-    qiblaDirection = calculateQiblaDirection(widget.userLatitude, widget.userLongitude);
-    _sensors.magnetometerEventStream().listen((event) {
-      setState(() {
-        initialDirection = calculateDeviceDirection(event);
-      });
-    });
   }
 
-  /// Calculate device direction
-  double calculateDeviceDirection(MagnetometerEvent event) {
-    // Manyetik alan verilerini kullanarak cihazın yönünü hesaplayın
-    double angle = atan2(event.y, event.x) * (180 / pi);
-    return (angle + 360) % 360; // Negatif açıları düzelt
-  }
-
-  /// Calculate Qibla direction
-  double calculateQiblaDirection(double latitude, double longitude) {
-    const double makkahLatitude = 21.4225;
-    const double makkahLongitude = 39.8262;
-
-    double latDifference = (makkahLatitude - latitude).toRadians();
-    double lonDifference = (makkahLongitude - longitude).toRadians();
-
-    double y = sin(lonDifference) * cos(makkahLatitude.toRadians());
-    double x = cos(latitude.toRadians()) * sin(makkahLatitude.toRadians()) - sin(latitude.toRadians()) * cos(makkahLatitude.toRadians()) * cos(lonDifference);
-
-    /// Kıble yönü hesaplamasında, kullanıcının bulunduğu konum ile Mekke arasındaki coğrafi
-    /// farktan bir açı elde etmek için atan2 fonksiyonu kullanılır.
-    /// Bu açı, cihazın hangi yöne bakması gerektiğini belirler.
-    double qiblaDirection = atan2(y, x).toDegrees();
-
-    return (qiblaDirection + 360) % 360;
+  @override
+  void dispose() {
+    _animationControllerForNeedle!.dispose();
+    _animationControllerForBackgroundCompass!.dispose();
+    super.dispose();
   }
 
   @override
@@ -99,34 +95,118 @@ class _PiriQiblahState extends State<PiriQiblah> {
     return Scaffold(
       appBar: AppBar(title: const Text('Piri Qiblah')),
       body: SafeArea(
-        child: qiblaDirection == null
-            ? const CircularProgressIndicator()
-            : Transform.rotate(
-                angle: (initialDirection - qiblaDirection!) * pi / 180,
-                child: Stack(
-                  children: [
-                    Center(
-                      child: SvgPicture.asset(
-                        'assets/compass.svg',
-                        width: 300,
-                        height: 300,
-                      ),
-                    ),
-                    Center(
-                      child: Transform.rotate(
-                        angle: qiblaDirection! * pi / 180 * -1,
-                        child: const Icon(
-                          Icons.navigation,
-                          size: 100,
-                          color: Colors.green,
-                        ),
-                      ),
-                    ),
-                  ],
+        child: SizedBox.expand(
+          child: Column(
+            children: [
+              const Spacer(flex: 30),
+              const Icon(
+                Icons.navigation,
+              ),
+              Expanded(
+                flex: 40,
+                child: StreamBuilder(
+                  stream: FlutterQiblah.qiblahStream,
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                        return _exceptionMessage();
+                      case ConnectionState.waiting:
+                        return _loadingIndicator();
+                      case ConnectionState.active:
+                        if (snapshot.hasError) {
+                          return _exceptionMessage();
+                        } else {
+                          /// Animation properties for needle view
+                          animationForNeedle = Tween(
+                            begin: beginForNeedle,
+                            end: (snapshot.data!.qiblah).toRadians() * -1,
+                          ).animate(_animationControllerForNeedle!);
+                          beginForNeedle = snapshot.data!.qiblah.toRadians() * -1;
+                          _animationControllerForNeedle!.forward(from: 0);
+
+                          /// Animation properties for background compass view
+                          animationForBackgroundCompass = Tween(
+                            begin: (snapshot.data!.direction).toRadians() * -1,
+                            end: 360.0,
+                          ).animate(_animationControllerForBackgroundCompass!);
+                          beginForCompass = snapshot.data!.direction.toRadians() * -1;
+                          _animationControllerForBackgroundCompass!.forward(from: 0);
+
+                          /// Return the compass view
+                          return Stack(
+                            children: [
+                              AnimatedBuilder(
+                                animation: animationForBackgroundCompass!,
+                                builder: (context, child) => Transform.rotate(
+                                  angle: animationForBackgroundCompass!.value,
+                                  child: Center(
+                                    child: SizedBox(
+                                      height: 300,
+                                      width: 300,
+                                      child: SvgPicture.asset('assets/compass.svg'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              AnimatedBuilder(
+                                animation: animationForNeedle!,
+                                builder: (context, child) => Transform.rotate(
+                                  angle: animationForNeedle!.value,
+                                  child: const Center(
+                                    child: Icon(
+                                      Icons.navigation,
+                                      size: 100,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                      case ConnectionState.done:
+                        return _exceptionMessage();
+                    }
+                  },
                 ),
               ),
+              const Spacer(flex: 30),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Widget _loadingIndicator() => const Center(child: CircularProgressIndicator());
+
+  Widget _exceptionMessage() => const Text(
+        'Something went wrong',
+      );
+
+  /// Request location permission for qiblah
+  Future<void> requestLocationPermissionForQiblah() async {
+    final permission = await checkLocationPermissionForQiblah();
+    if (!permission) {
+      await FlutterQiblah.requestPermissions();
+    }
+  }
+
+  /// Check location permission for qiblah
+  Future<bool> checkLocationPermissionForQiblah() async {
+    final permission = await Geolocator.checkPermission();
+    switch (permission) {
+      case LocationPermission.denied:
+        return false;
+      case LocationPermission.deniedForever:
+        return false;
+      case LocationPermission.whileInUse:
+        return true;
+      case LocationPermission.always:
+        return true;
+      case LocationPermission.unableToDetermine:
+        return false;
+    }
   }
 }
 
--- lib/src/piri_qiblah.dart
+++ lib/src/piri_qiblah.dart
@@ -1,67 +1,135 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:sensors_plus/sensors_plus.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
 
 /// PiriQiblah is a widget that shows the direction of the Qiblah.
 @immutable
 final class PiriQiblah extends StatefulWidget {
-  ///
-  const PiriQiblah({
-    super.key,
-    required this.langitude,
-    required this.longitude,
-  });
-
-  /// The langitude and longitude
-  final double langitude;
-  final double longitude;
+  const PiriQiblah({super.key});
 
   @override
   State<PiriQiblah> createState() => _PiriQiblahState();
 }
 
-class _PiriQiblahState extends State<PiriQiblah> {
-  double _direction = 0;
+class _PiriQiblahState extends State<PiriQiblah> with TickerProviderStateMixin {
+  /// --------------------------------------------------------------------------
+  /// Animation properties for needles
+  late Animation<double>? _animationForNeedle;
+  late AnimationController? _animationControllerForNeedle;
+
+  /// --------------------------------------------------------------------------
+  /// Animation properties for background compass view
+  late Animation<double>? _animationForBackgroundCompass;
+  late AnimationController? _animationControllerForBackgroundCompass;
+
+  /// --------------------------------------------------------------------------
+  /// Initial tween begin values
+  double beginForNeedle = 0;
+  double beginForCompass = 0;
 
   @override
   void initState() {
     super.initState();
-    Sensors().magnetometerEventStream().listen((MagnetometerEvent event) {
-      setState(() {
-        _direction = event.x;
-      });
-    });
+
+    /// --------------------------------------------------------------------------
+    /// Animation controller for needles
+    _animationControllerForNeedle = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    /// --------------------------------------------------------------------------
+    /// Tween for needle view
+    _animationForNeedle = Tween<double>(
+      begin: 0.0,
+      end: 0.0,
+    ).animate(_animationControllerForNeedle!);
+
+    /// --------------------------------------------------------------------------
+    /// Animation controller for background  compass view
+    _animationControllerForBackgroundCompass = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 500),
+    );
+
+    /// --------------------------------------------------------------------------
+    /// Tween for background compass view
+    _animationForBackgroundCompass = Tween<double>(
+      begin: 0.0,
+      end: 0.0,
+    ).animate(_animationControllerForBackgroundCompass!);
+  }
+
+  @override
+  void dispose() {
+    _animationControllerForNeedle!.dispose();
+    _animationControllerForBackgroundCompass!.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Transform.rotate(
-          angle: _direction * pi / 180 * -1,
+    return StreamBuilder(
+      stream: FlutterQiblah.qiblahStream,
+      builder: (context, snapshot) {
+        /// Animation properties for needle view
+        _animationForNeedle = Tween(
+          begin: beginForNeedle,
+          end: (snapshot.data!.qiblah).toRadians() * -1,
+        ).animate(_animationControllerForNeedle!);
+        beginForNeedle = snapshot.data!.qiblah * (pi / 180) * -1;
+        _animationControllerForNeedle!.forward(from: 0);
+
+        /// Animation properties for background compass view
+        _animationForBackgroundCompass = Tween(
+          begin: (snapshot.data!.direction).toRadians() * -1,
+          end: 360.0,
+        ).animate(_animationControllerForBackgroundCompass!);
+        beginForCompass = snapshot.data!.direction * (pi / 180) * -1;
+        _animationControllerForBackgroundCompass!.forward(from: 0);
+
+        /// Return the compass view
+        return Stack(
+          children: [
+            _backgroundCompassView(),
+            _needleView(),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _needleView() {
+    return AnimatedBuilder(
+      animation: _animationForNeedle!,
+      builder: (context, child) => Transform.rotate(
+        angle: _animationForNeedle!.value,
+        child: const Center(
           child: Icon(
             Icons.navigation,
             size: 100,
+            color: Colors.green,
           ),
         ),
       ),
     );
   }
-}
 
-double calculateQiblaDirection(double latitude, double longitude) {
-  const double makkahLatitude = 21.4225;
-  const double makkahLongitude = 39.8262;
-
-  double lonDifference = (makkahLongitude - longitude).toRadians();
-
-  double y = sin(lonDifference) * cos(makkahLatitude.toRadians());
-  double x = cos(latitude.toRadians()) * sin(makkahLatitude.toRadians()) - sin(latitude.toRadians()) * cos(makkahLatitude.toRadians()) * cos(lonDifference);
-
-  double qiblaDirection = atan2(y, x).toDegrees();
-
-  return (qiblaDirection + 360) % 360;
+  Widget _backgroundCompassView() {
+    return AnimatedBuilder(
+      animation: _animationForBackgroundCompass!,
+      builder: (context, child) => Transform.rotate(
+        angle: _animationForBackgroundCompass!.value,
+        child: const Center(
+          child: SizedBox(
+            height: 300,
+            width: 300,
+            child: Icon(Icons.compass_calibration),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 extension AngleConversion on double {
